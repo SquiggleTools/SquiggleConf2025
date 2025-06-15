300cc2d39a31a479eb853ca2fef0ef4d8975db11
Josh Goldberg <git@joshuakgoldberg.com>
2025-06-15 15:19:50 -0400

    feat: added 2024 contents under /2024

--- astro.config.ts
+++ astro.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
 	integrations: [konamiEmojiBlast()],
 	output: "server",
 	redirects: {
+		"/2024/about": "/about",
+		"/2024/code-of-conduct": "/code-of-conduct",
+		"/2024/faqs": "/faqs",
+		"/2024/travel": "/travel",
 		"/how-to-attend-squiggleconf-for-free":
 			"/articles/how-to-attend-squiggleconf-for-free",
 	},
