c13eb3ac9b7f4dc6878efa0847bfbc44b63282f5
Josh Goldberg <git@joshuakgoldberg.com>
2025-07-01 10:12:50 -0400

    fix: redirect /2025 to /

--- astro.config.ts
+++ astro.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
 		"/2024/code-of-conduct": "/code-of-conduct",
 		"/2024/faqs": "/faqs",
 		"/2024/travel": "/travel",
+		"/2025": "/",
 		"/faqs": "/articles/faqs",
 		"/how-to-attend-squiggleconf-for-free":
 			"/articles/how-to-attend-squiggleconf-for-free",
