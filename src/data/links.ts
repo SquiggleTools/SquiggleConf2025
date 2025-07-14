f5bb61fabfde08d35544f013d54eead748751477
Josh Goldberg <git@joshuakgoldberg.com>
2025-07-14 12:50:06 -0400

    feat: donation button in /about

--- src/data/links.ts
+++ src/data/links.ts
@@ -5,6 +5,7 @@ export const links = {
 		"https://discord.com/channels/1181983343387099207/1244808425762848938",
 	cfpQuestionsEmail: "mailto:cfp@squiggleconf.com",
 	discord: "https://discord.squiggleconf.com",
+	donate: "",
 	michiganTypeScript: "https://youtube.com/@MichiganTypeScript",
 	shop: "https://shop.squiggle.tools",
 	tickets: "https://buytickets.at/squiggleconf/1488622",
