efe00c552b132ec171779a5741f5ce38cba8453e
Josh Goldberg <git@joshuakgoldberg.com>
2025-07-21 12:07:29 -0400

    feat: added JSHeroes to Partners

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -2,6 +2,8 @@ import afhBlack from "~/assets/logos/afh-black.svg";
 import afhWhite from "~/assets/logos/afh-white.svg";
 import bostonTSClubDark from "~/assets/logos/boston-ts-club-full-dark.svg";
 import bostonTSClubLight from "~/assets/logos/boston-ts-club-full-light.svg";
+import jsheroesDarkMode from "~/assets/logos/jsheroes-logo-dark.svg";
+import jsheroesLightMode from "~/assets/logos/jsheroes-logo-light.svg";
 import michiganTypeScript from "~/assets/logos/michigan-typescript.png";
 
 export const partnersBySlug = {
@@ -20,6 +22,14 @@ export const partnersBySlug = {
 			light: bostonTSClubLight,
 		},
 		title: "Boston TS Club",
+	},
+	jsheroes: {
+		href: "https://jsheroes.io?utm_source=squiggleconf",
+		logos: {
+			dark: jsheroesDarkMode,
+			light: jsheroesLightMode,
+		},
+		title: "JSHeroes",
 	},
 	michiganTypeScript: {
 		href: "https://www.youtube.com/@MichiganTypeScript?utm_source=squiggleconf",
