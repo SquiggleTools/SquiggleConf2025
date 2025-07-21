d5b4ae285628ac4b29c0dd3e878bac8f6e0b69fc
Josh Goldberg <git@joshuakgoldberg.com>
2025-07-21 12:07:04 -0400

    chore: remove commented-out partners

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -3,14 +3,6 @@ import afhWhite from "~/assets/logos/afh-white.svg";
 import bostonTSClubDark from "~/assets/logos/boston-ts-club-full-dark.svg";
 import bostonTSClubLight from "~/assets/logos/boston-ts-club-full-light.svg";
 import michiganTypeScript from "~/assets/logos/michigan-typescript.png";
-// import jsheroesDarkMode from "~/assets/logos/jsheroes-dark-mode.png";
-// import jsheroesLightMode from "~/assets/logos/jsheroes-light-mode.png";
-// import reactAfricaDark from "~/assets/logos/react-africa-dark.png";
-// import reactAfricaLight from "~/assets/logos/react-africa-light.webp";
-// import reactMiamiDarkMode from "~/assets/logos/react-miami-dark-mode.png";
-// import reactMiamiLightMode from "~/assets/logos/react-miami-light-mode.png";
-// import resilientCodersDarkMode from "~/assets/logos/resilient-coders-dark-mode.png";
-// import resilientCodersLightMode from "~/assets/logos/resilient-coders-light-mode.png";
 
 export const partnersBySlug = {
 	afh: {
@@ -37,36 +29,4 @@ export const partnersBySlug = {
 		},
 		title: "Michigan TypeScript",
 	},
-	// cascadiaJs: {
-	// 	href: "https://jsheroes.com?utm_source=squiggleconf",
-	// 	logos: {
-	// 		dark: jsheroesDarkMode,
-	// 		light: jsheroesLightMode,
-	// 	},
-	// 	title: "This Week In React",
-	// },
-	// reactAfrica: {
-	// 	href: "https://react-africa.com/?utm_source=squiggleconf",
-	// 	logos: {
-	// 		dark: reactAfricaLight,
-	// 		light: reactAfricaDark,
-	// 	},
-	// 	title: "React Africa",
-	// },
-	// resilientCoders: {
-	// 	href: "https://resilient-coders.com/?utm_source=squiggleconf",
-	// 	logos: {
-	// 		dark: resilientCodersLight,
-	// 		light: resilientCodersDark,
-	// 	},
-	// 	title: "Resilient Coders",
-	// },
-	// reactMiami: {
-	// 	href: "https://reactmiami.com?utm_source=squiggleconf",
-	// 	logos: {
-	// 		dark: reactMiamiDarkMode,
-	// 		light: reactMiamiLightMode,
-	// 	},
-	// 	title: "React Miami",
-	// },
 };
