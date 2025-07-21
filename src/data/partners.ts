3d5c159c24e80d4cb6040590c627635fd8e974a8
Josh Goldberg <git@joshuakgoldberg.com>
2025-07-21 15:15:33 -0400

    feat: add Magnolia(JS)Conf to partners

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -2,6 +2,8 @@ import afhBlack from "~/assets/logos/afh-black.svg";
 import afhWhite from "~/assets/logos/afh-white.svg";
 import bostonTSClubDark from "~/assets/logos/boston-ts-club-full-dark.svg";
 import bostonTSClubLight from "~/assets/logos/boston-ts-club-full-light.svg";
+import magnoliaConfColored from "~/assets/logos/magnoliaconf-colored.svg";
+import magnoliaConfWhite from "~/assets/logos/magnoliaconf-white.svg";
 import michiganTypeScript from "~/assets/logos/michigan-typescript.png";
 
 export const partnersBySlug = {
@@ -20,6 +22,14 @@ export const partnersBySlug = {
 			light: bostonTSClubLight,
 		},
 		title: "Boston TS Club",
+	},
+	magnoliaConf: {
+		href: "https://2025.magnoliaconf.com?utm_source=squiggleconf",
+		logos: {
+			dark: magnoliaConfWhite,
+			light: magnoliaConfColored,
+		},
+		title: "Magnolia JS Conf",
 	},
 	michiganTypeScript: {
 		href: "https://www.youtube.com/@MichiganTypeScript?utm_source=squiggleconf",
